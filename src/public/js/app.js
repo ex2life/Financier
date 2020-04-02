bf533a24215f7513d369cb10bd66543c411c68b9
Sergey Abramov <abramizsaransk@gmail.com>
2020-04-02 17:16:35 +0300

    fix envoiroment replace mysql fix ports

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -49661,8 +49661,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/site/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/site/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })

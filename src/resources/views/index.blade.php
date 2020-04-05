663d5b36a60d73a9a036a5e70367a465bb4d3e5e
Sergey Abramov <abramizsaransk@gmail.com>
2020-04-05 16:47:58 +0300

    btn fix

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,16 +5,17 @@
         <header>
             <h1 class="text-center">ФИНАНСИСТ ОНЛАЙН</h1>
         </header>
-            <a class="btn_margin btn btn-light btn-lg col-12 col-md-4 offset-md-8" href="">Кредитный
+        <div class="row mt-5">
+            <a class="mt-3 btn btn-light btn-lg col-10 offset-1 col-md-4 offset-md-8" href="">Кредитный
                 калькулятор </a>
-            <div class="w-100"></div>
-            <a class="btn_margin btn btn btn-light btn-lg col-12 col-md-4 offset-md-8"
+            <a class="mt-3 btn btn btn-light btn-lg col-10 offset-1 col-md-4 offset-md-8"
                href="">Расчет суммы кредита</a>
-            <a class="btn_margin btn btn-light btn-lg col-12 col-md-4 offset-md-8"
+            <a class="mt-3 btn btn-light btn-lg col-10 offset-1 col-md-4 offset-md-8"
                href="#">Финансовый анализ</a>
-            <a class="btn_margin btn btn-light btn-lg col-12 col-md-4 offset-md-8"
+            <a class="mt-3 btn btn-light btn-lg col-10 offset-1 col-md-4 offset-md-8"
                href="#">Управленческая отчетность</a>
-            <a class="btn_margin btn btn-light btn-lg col-12 col-md-4 offset-md-8"
+            <a class="mt-3 btn btn-light btn-lg col-10 offset-1 col-md-4 offset-md-8"
                href="#">Анализ инвестиционных проектов</a>
+        </div>
     </div>
 @endsection

98ca7d813e22deb112f4bad2f09883bef5c50a64
Sergey Abramov <abramizsaransk@gmail.com>
2020-04-06 01:30:23 +0300

    input param kredit

--- src/resources/views/calc/input.blade.php
+++ src/resources/views/calc/input.blade.php
@@ -2,56 +2,54 @@
 
 @section('content')
     <div class="container">
-        <div class="header">
-            <?php
-            if ($type == 'annuit')
-                echo '<h2 class="text-center">АННУИТЕТНЫЙ <br>ПЛАТЕЖ</h2>';
-            elseif ($type == 'differ')
-                echo '<h2 class="text-center">ДИФФЕРЕНЦИРОВАННЫЙ <br>ПЛАТЕЖ</h2>';
-            elseif ($type == 'flex')
-                echo '<h2 class="text-center">ГИБКИЙ ГРАФИК<br>ПОГАШЕНИЯ</h2>';
-            ?>
-        </div>
-        <div class="jumbotron opacity">
-            <form method="post" id="frmPlatezhParam" action="out_platezh_schedule.php">
-                <?php
-                if ($type == 'annuit')
-                    echo '<input type="hidden" name="type_platezh" id="type_platezh" value="annuit">';
-                elseif ($type == 'differ')
-                    echo '<input type="hidden" name="type_platezh" id="type_platezh" value="differ">';
-                elseif ($type == 'flex')
-                    echo '<input type="hidden" name="type_platezh" id="type_platezh" value="flex">';
-                ?>
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div class="form-group">
+        <div class="card mt-5 opacitybg">
+            <div class="card-header">
+                <h2 class="text-center">{{($type == 'annuit') ? 'АННУИТЕТНЫЙ ПЛАТЕЖ' : (($type == 'differ') ? 'ДИФФЕРЕНЦИРОВАННЫЙ ПЛАТЕЖ' : 'ГИБКИЙ ГРАФИК ПОГАШЕНИЯ')}}</h2>
+            </div>
+            <div class="card-body">
+                <form action="" id="frmPlatezhParam" method="post">
+                    <input type="hidden" name="type_platezh" id="type_platezh" value="{{$type}}">
+                    <div class="row">
+                        <div class="col-12 col-md-6">
                             <label for="str_beg_date" class="control-label text-left">Дата получения кредита
                                 (ММ.ГГГГ) </label>
-                        <!-- Сначала дату вводили в полном формате
-							<input type="date" name="str_beg_date" value="<?=date('d.m.Y')?>" maxlength="10" autofocus required pattern="(0[1-9]|1[0-9]|2[0-9]|3[01])\.(0[1-9]|1[012])\.[0-9]{4}"> -->
                             <input type="text" class="form-control" name="str_beg_date" value="<?=date('m.Y')?>"
                                    id="str_beg_date" maxlength="7" autofocus required
                                    pattern="(0[1-9]|1[012])\.[0-9]{4}">
                         </div>
-                        <div class="form-group">
-                            <label for="sum_kred" class="control-label">Сумма кредита </label>
-                            <input type="text" class="form-control" name="sum_kred" id="sum_kred" required
-                                   pattern="^\d+(\.\d{1,2})?$">
+                        <div class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="col_month" class="control-label">Срок кредита в месяцах </label>
+                                <input type="number" class="form-control" min="1" name="col_month" id="col_month"
+                                       value="1"
+                                       required>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                            <label for="col_month" class="control-label">Срок кредита в месяцах </label>
-                            <input type="number" class="form-control" min="1" name="col_month" id="col_month" value="1"
-                                   required>
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="sum_kred" class="control-label">Сумма кредита </label>
+                                <input type="text" class="form-control" name="sum_kred" id="sum_kred" required
+                                       pattern="^\d+(\.\d{1,2})?$">
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="proc" class="control-label">Процентная ставка в год </label>
-                            <input type="text" class="form-control" name="proc" id="proc" required
-                                   pattern="^\d+(\.\d{1,2})?$">
+                        <div class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="proc" class="control-label">Процентная ставка в год </label>
+                                <input type="text" class="form-control" name="proc" id="proc" required
+                                       pattern="^\d+(\.\d{1,2})?$">
+                            </div>
                         </div>
-                        <!-- <a href="#text-popup" class="popup-content">Вызвать окно с текстом</a>  -->
                     </div>
+                </form>
+            </div>
+        </div>
+
+
+        <div class="jumbotron opacitybg">
+            <form method="post" id="frmPlatezhParam" action="out_platezh_schedule.php">
+                <div class="row">
                     <div class="col-sm-6">
                         <?php
                         if ($type == 'flex') {

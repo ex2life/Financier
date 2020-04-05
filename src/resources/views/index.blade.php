8f049b665df10ad86f5f9e386a09c6a6dc756bc9
Sergey Abramov <abramizsaransk@gmail.com>
2020-04-05 18:01:43 +0300

    end stock auth

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -2,9 +2,6 @@
 
 @section('content')
     <div class="container">
-        <header>
-            <h1 class="text-center">ФИНАНСИСТ ОНЛАЙН</h1>
-        </header>
         <div class="row mt-5">
             <a class="mt-3 btn btn-light btn-lg col-10 offset-1 col-md-4 offset-md-8" href="">Кредитный
                 калькулятор </a>
--- src/routes/web.php
+++ src/routes/web.php
@@ -17,7 +17,6 @@ Route::get('/', function () {
     return view('index');
 });
 
-//test windows
 
 Auth::routes();
 
